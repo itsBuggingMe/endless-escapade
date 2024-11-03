ab0e69e1aae1ed4d28c0368bcc9a46629bf8805a
shnakamura <joaovtnakamura@gmail.com>
2024-11-02 23:39:34 -0300

    Adds assets for hatchets & daggers

--- src/EndlessEscapade/Common/Projectiles/WoodenArrowGlobalProjectile.cs
+++ src/EndlessEscapade/Common/Projectiles/WoodenArrowGlobalProjectile.cs
@@ -2,6 +2,18 @@ using EndlessEscapade.Utilities.Extensions;
 
 namespace EndlessEscapade.Common.Projectiles;
 
+/// <summary>
+///     <para>
+///         Handles the transformation behavior of wooden arrows when they are submerged in specific liquids.
+///     </para>
+///     <para>
+///         When submerged in <see cref="LiquidID.Lava"/>, wooden arrows will transform into
+///         <see cref="ProjectileID.FireArrow"/>.
+///         <br />
+///         When submerged in <see cref="LiquidID.Shimmer"/>, wooden arrows will transform into
+///         <see cref="ProjectileID.ShimmerArrow"/>.
+///     </para>
+/// </summary>
 public sealed class WoodenArrowGlobalProjectile : GlobalProjectile
 {
     public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) {
