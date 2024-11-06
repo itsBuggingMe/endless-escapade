ce7276317b6447471d7f38e4d9bc0c4cbd6ff24e
shnakamura <joaovtnakamura@gmail.com>
2024-11-06 14:38:42 -0300

    Separates all EC into another branch

--- src/EndlessEscapade/Core/EC/_Attributes/AfterAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace EndlessEscapade.Core.EC;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public sealed class AfterAttribute(Type type) : Attribute
-{
-    public readonly Type Type = type;
-}
--- src/EndlessEscapade/Core/EC/_Attributes/RequiresAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace EndlessEscapade.Core.EC;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public sealed class RequiresAttribute(Type type) : Attribute
-{
-    public readonly Type Type = type;
-}
