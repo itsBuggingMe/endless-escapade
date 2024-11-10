c195d6a79d8084b0412c4ee7cfc34d60b7d59bba
shnakamura <joaovtnakamura@gmail.com>
2024-11-10 01:40:04 -0300

    Cleans up inventory item effects

--- src/EndlessEscapade/Core/Graphics/_Items/InventoryGraphicsGlobalItem.cs
+++ src/EndlessEscapade/Core/Graphics/_Items/InventoryGraphicsGlobalItem.cs
@@ -7,6 +7,7 @@ using Terraria.UI.Chat;
 
 namespace EndlessEscapade.Core.Graphics;
 
+[Autoload(Side = ModSide.Client)]
 public sealed class InventoryGraphicsGlobalItem : GlobalItem
 {
     private const float SELECTED_ITEM_SCALE = 1f;
@@ -17,39 +18,20 @@ public sealed class InventoryGraphicsGlobalItem : GlobalItem
         PitchVariance = 0.2f
     };
 
+    private float inventoryDrawScale;
+    private float inventoryDrawRotation;
+
+    private Vector2 inventoryDrawPosition;
+
+    // TODO: Maybe store this somewhere else for general use? Same for Main.MouseWorld.
+    private Vector2 oldMouseScreen;
+
     public override bool InstancePerEntity { get; } = true;
-
-    public float Scale { get; set; }
-    public float Rotation { get; set; }
-
-    public Vector2 Position;
-    public Vector2 OldMouseScreen;
-
-    public bool Hovering;
-    public bool OldHovering;
 
     public override void Load() {
         base.Load();
 
-        On_ItemSlot.MouseHover_ItemArray_int_int += ItemSlot_MouseHover_Hook;
         On_ItemSlot.DrawItemIcon += ItemSlot_DrawItemIcon_Hook;
-    }
-
-    public override bool PreDrawInInventory(
-        Item item,
-        SpriteBatch spriteBatch,
-        Vector2 position,
-        Rectangle frame,
-        Color drawColor,
-        Color itemColor,
-        Vector2 origin,
-        float scale
-    ) {
-        return base.PreDrawInInventory(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
-    }
-
-    private static void ItemSlot_MouseHover_Hook(On_ItemSlot.orig_MouseHover_ItemArray_int_int orig, Item[] inv, int context, int slot) {
-        orig(inv, context, slot);
     }
 
     private static float ItemSlot_DrawItemIcon_Hook(
@@ -94,35 +76,29 @@ public sealed class InventoryGraphicsGlobalItem : GlobalItem
             40
         );
 
-        graphics.Hovering = rectangle.Contains(Main.MouseScreen.ToPoint());
-
-        if (graphics.Hovering && !graphics.OldHovering) {
-            SoundEngine.PlaySound(in InventoryTickSound);
-        }
-
-        graphics.OldHovering = graphics.Hovering;
+        var hovering = rectangle.Contains(Main.MouseScreen.ToPoint());
 
         if (context == ItemSlot.Context.MouseItem) {
-            var velocity = Main.MouseScreen - graphics.OldMouseScreen;
+            var velocity = Main.MouseScreen - graphics.oldMouseScreen;
             var rotation = velocity.X * 0.01f;
 
-            graphics.OldMouseScreen = Main.MouseScreen;
+            graphics.oldMouseScreen = Main.MouseScreen;
 
-            graphics.Rotation = MathHelper.Lerp(graphics.Rotation, rotation, 0.1f);
+            graphics.inventoryDrawRotation = MathHelper.Lerp(graphics.inventoryDrawRotation, rotation, 0.1f);
 
             var targetScale = SELECTED_ITEM_SCALE + (velocity.Length() * 0.005f);
 
-            graphics.Scale = MathHelper.Lerp(graphics.Scale, targetScale, 0.1f);
+            graphics.inventoryDrawScale = MathHelper.Lerp(graphics.inventoryDrawScale, targetScale, 0.1f);
         }
         else {
-            graphics.Scale = MathHelper.SmoothStep(graphics.Scale, graphics.Hovering ? SELECTED_ITEM_SCALE : UNSELECTED_ITEM_SCALE, 0.5f);
+            graphics.inventoryDrawScale = MathHelper.SmoothStep(graphics.inventoryDrawScale, hovering ? SELECTED_ITEM_SCALE : UNSELECTED_ITEM_SCALE, 0.5f);
 
-            scale = graphics.Scale;
+            scale = graphics.inventoryDrawScale;
         }
 
-        graphics.Position = Vector2.SmoothStep(graphics.Position, screenPositionForItemCenter, 0.5f);
+        graphics.inventoryDrawPosition = Vector2.SmoothStep(graphics.inventoryDrawPosition, screenPositionForItemCenter, 0.5f);
 
-        screenPositionForItemCenter = graphics.Position;
+        screenPositionForItemCenter = graphics.inventoryDrawPosition;
 
         if (context != ItemSlot.Context.MouseItem) {
             return orig(item, context, spriteBatch, screenPositionForItemCenter, scale, sizeLimit, environmentColor);
@@ -134,10 +110,10 @@ public sealed class InventoryGraphicsGlobalItem : GlobalItem
             texture,
             screenPositionForItemCenter,
             frame,
-            Color.White,
-            graphics.Rotation,
+            environmentColor,
+            graphics.inventoryDrawRotation,
             frame.Size() / 2f,
-            graphics.Scale,
+            graphics.inventoryDrawScale,
             SpriteEffects.None,
             0f
         );
