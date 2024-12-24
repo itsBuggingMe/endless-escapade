ad4516eebfb1f7ceb6c5cb93b958bcf4018c9d3c
shnakamura <joaovtnakamura@gmail.com>
2024-12-24 20:18:58 -0300

    feat: makes armor sets all in one file

--- src/EndlessEscapade/Content/Items/Tropical/TropicalArmorSet.cs
+++ src/EndlessEscapade/Content/Items/Tropical/TropicalArmorSet.cs
@@ -43,3 +43,49 @@ public class TropicalWoodHelmetItem : ModItem
         return body.type == ModContent.ItemType<TropicalWoodChestplateItem>() && legs.type == ModContent.ItemType<TropicalWoodBootsItem>();
     }
 }
+
+[AutoloadEquip(EquipType.Body)]
+public class TropicalWoodChestplateItem : ModItem
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        Item.defense = 3;
+
+        Item.width = 24;
+        Item.height = 22;
+    }
+
+    public override void AddRecipes()
+    {
+        base.AddRecipes();
+
+        CreateRecipe()
+            .AddIngredient<TropicalWoodItem>(30)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
+
+[AutoloadEquip(EquipType.Legs)]
+public class TropicalWoodBootsItem : ModItem
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        Item.defense = 2;
+
+        Item.width = 18;
+        Item.height = 12;
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient<TropicalWoodItem>(25)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
