261c3d84807a12d015142e278edaadf695381fc5
shnakamura <joaovtnakamura@gmail.com>
2024-12-26 03:31:35 -0300

    feat: adds cheese

---
+++ src/EndlessEscapade/Content/Items/Miscellaneous/CheeseItem.cs
@@ -0,0 +1,23 @@
+﻿namespace EndlessEscapade.Content.Items.Miscellaneous;
+
+public class CheeseItem : ModItem
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        Item.maxStack = Item.CommonMaxStack;
+
+        Item.consumable = true;
+        
+        Item.width = 50;
+        Item.height = 34;
+    }
+
+    public override bool ConsumeItem(Player player)
+    {
+        player.AddBuff(BuffID.WellFed, 10 * 60 * 60);
+        
+        return base.ConsumeItem(player);
+    }
+}
