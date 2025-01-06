bcbc2234b3c30ad8e9dcf28fd1143d0fdec92f8d
shnakamura <joaovtnakamura@gmail.com>
2025-01-06 04:33:15 -0300

    feat: adds vanilla item recipes

---
+++ src/EndlessEscapade/Common/Recipes/_Vanilla/ConsumableRecipesSystem.cs
@@ -0,0 +1,21 @@
+﻿namespace EndlessEscapade.Common.Recipes;
+
+public sealed class ConsumableRecipesSystem : GlobalItem
+{
+    public override void AddRecipes() 
+    {
+        base.AddRecipes();
+
+        Recipe.Create(ItemID.MagicMirror)
+            .AddIngredient(ItemID.Glass, 20)
+            .AddIngredient(ItemID.FallenStar, 3)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+
+        Recipe.Create(ItemID.IceMirror)
+            .AddIngredient(ItemID.Glass, 20)
+            .AddIngredient(ItemID.FallenStar, 3)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
