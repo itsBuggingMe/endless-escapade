406dfca79dbca8a0672b1153fb1e2b600a5b02a3
shnakamura <joaovtnakamura@gmail.com>
2025-01-07 03:14:18 -0300

    feat: assets of old content

---
+++ src/EndlessEscapade/Content/Items/Food/KelpberryShakeItem.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.DataStructures;
+
+namespace EndlessEscapade.Content.Items.Food;
+
+public class KelpberryShakeItem : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        base.SetStaticDefaults();
+        
+        ItemID.Sets.IsFood[Type] = true;
+        
+        Main.RegisterItemAnimation(Type, new DrawAnimationVertical(int.MaxValue, 3));
+    }
+
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        
+        Item.DefaultToFood(16, 30, BuffID.WellFed, 10 * 60 * 60);
+    }
+}
