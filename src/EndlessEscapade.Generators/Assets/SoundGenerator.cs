042beb1ad674111f0deefb73c62aa893051f456e
shnakamura <joaovtnakamura@gmail.com>
2025-01-10 18:24:28 -0300

    feat: base api for asset generation

---
+++ src/EndlessEscapade.Generators/Assets/SoundGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace EndlessEscapade.Generators.Assets;
+
+[Generator(LanguageNames.CSharp)]
+public sealed class SoundGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        
+    }
+}
