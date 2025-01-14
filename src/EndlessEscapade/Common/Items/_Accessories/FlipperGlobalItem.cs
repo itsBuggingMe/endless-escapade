f3ddca3622895c848b8532d903e69ee6d32aaf3f
shnakamura <joaovtnakamura@gmail.com>
2025-01-14 18:00:44 -0300

    feat: splits oxygen hooks

--- src/EndlessEscapade/Common/Items/_Accessories/FlipperGlobalItem.cs
+++ src/EndlessEscapade/Common/Items/_Accessories/FlipperGlobalItem.cs
@@ -1,20 +1,22 @@
-using EndlessEscapade.Common.Movement;
 using EndlessEscapade.Common.Swimming;
 
 namespace EndlessEscapade.Common.Items;
 
 public sealed class FlipperGlobalItem : GlobalItem
 {
-    public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
+    public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+    {
         return entity.type == ItemID.Flipper;
     }
 
-    public override void UpdateEquip(Item item, Player player) {
+    public override void UpdateEquip(Item item, Player player)
+    {
         base.UpdateEquip(item, player);
 
         player.accFlipper = false;
 
-        if (!player.TryGetModPlayer(out SwimmingPlayer swimmingPlayer)) {
+        if (!player.TryGetModPlayer(out SwimmingPlayer swimmingPlayer))
+        {
             return;
         }
 
