90ea226be4676c24f939419415b173e0a783f5bb
shnakamura <joaovtnakamura@gmail.com>
2025-01-15 00:02:34 -0300

    docs: sprite documentation

--- src/EndlessEscapade/Core/Graphics/_Rendering/_Elements/Sprite.cs
+++ src/EndlessEscapade/Core/Graphics/_Rendering/_Elements/Sprite.cs
@@ -5,48 +5,51 @@ namespace EndlessEscapade.Core.Graphics;
 public struct Sprite
 {
     /// <summary>
-    /// 
+    ///     Gets or sets the texture of the sprite, as an asset.
     /// </summary>
     public Asset<Texture2D> Texture { readonly get; set; }
 
     /// <summary>
-    /// 
+    ///     Gets or sets the position of the sprite, in screen coordinates.
     /// </summary>
     public Vector2 Position { readonly get; set; }
 
     /// <summary>
-    ///     
+    ///     Gets or sets the scale of the sprite. Defaults to <see cref="Vector2.One"/>.
     /// </summary>
     public Vector2 Scale { readonly get; set; } = Vector2.One;
     
     /// <summary>
-    ///     
+    ///     Gets or sets the origin of the sprite.
     /// </summary>
-    public Vector2 Origin { readonly get; set; } = new(0.5f);
+    public Vector2 Origin { readonly get; set; }
     
     /// <summary>
-    /// 
+    ///     Gets or sets the effects of the sprite.
     /// </summary>
     public SpriteEffects Effects { readonly get; set; }
     
     /// <summary>
-    /// 
+    ///     Gets or sets the source rectangle of the sprite.
     /// </summary>
     public Rectangle? SourceRectangle { readonly get; set; }
     
     /// <summary>
-    /// 
+    ///     Gets or sets the destination rectangle of the sprite.
     /// </summary>
     public Rectangle? DestinationRectangle { readonly get; set; }
     
     /// <summary>
-    /// 
+    ///     Gets or sets the rotation of the sprite, in radians.
     /// </summary>
     public float Rotation { readonly get; set; }
 
     /// <summary>
-    /// 
+    ///     Gets or sets the opacity of the sprite, where 0 is fully transparent and 1 is fully opaque.
     /// </summary>
+    /// <remarks>
+    ///     This value is clamped between 0 and 1.
+    /// </remarks>
     public float Opacity
     {
         readonly get => _opacity;
@@ -54,7 +57,7 @@ public struct Sprite
     }
 
     /// <summary>
-    /// 
+    ///     Gets or sets the color of the sprite. Defaults to <see cref="Color.White"/>.
     /// </summary>
     public Color Color
     {
